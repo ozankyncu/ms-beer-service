118911bb803167dad0b5ccc31a337c0fb8f851bc
ozanko <ozankyncu@gmail.com>
2022-04-25 23:39:16 +0300

    Persistance clases implemented

---
+++ src/main/java/com/kyncu/msbeerservice/repository/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.kyncu.msbeerservice.repository;
+
+import com.kyncu.msbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
