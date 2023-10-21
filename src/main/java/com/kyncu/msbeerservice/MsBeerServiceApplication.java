d608951657d653d6f50b7faa53ee54970acac893
Ozan Koyuncu <ozankyncu@gmail.com>
2023-10-21 20:07:40 +0300

    Mysql configuration initialized

--- src/main/java/com/kyncu/msbeerservice/MsBeerServiceApplication.java
+++ src/main/java/com/kyncu/msbeerservice/MsBeerServiceApplication.java
@@ -2,8 +2,9 @@ package com.kyncu.msbeerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication( exclude = ArtemisAutoConfiguration.class)
 public class MsBeerServiceApplication {
 
 	public static void main(String[] args) {
