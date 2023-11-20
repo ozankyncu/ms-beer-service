b8ad39f9ba098176b4a1387d8e4f3fafa8dba137
Ozan Koyuncu <ozankyncu@gmail.com>
2023-11-20 21:58:20 +0300

    State machine configured

---
+++ target/generated-sources/annotations/com/kyncu/beersystem/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,15 @@
+package com.kyncu.beersystem.web.mappers;
+
+import javax.annotation.Generated;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-11-02T20:40:52+0300",
+    comments = "version: 1.5.0.Final, compiler: javac, environment: Java 17.0.6 (JetBrains s.r.o.)"
+)
+@Component
+@Primary
+public class BeerMapperImpl extends BeerMapperDecorator {
+}
